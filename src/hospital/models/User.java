9a94f95772dc9d93f3339147153684fa4af68409
tEkaterina <ekaterina.tselishcheva@gmail.com>
2016-05-06 22:31:17 +0300

    Add an user model.

---
+++ src/hospital/models/User.java
@@ -0,0 +1,54 @@
+package hospital.models;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "users")
+public class User {
+    public enum RoleName { Admin, Doctor }
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+
+    private String email;
+    private String password;
+    private String salt;
+    
+    private RoleName roleName;
+
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getSalt() {
+        return salt;
+    }
+    public void setSalt(String salt) {
+        this.salt = salt;
+    }
+
+    public RoleName getRoleName() {
+        return roleName;
+    }
+    public void setRoleName(RoleName roleName) {
+        this.roleName = roleName;
+    }
+}
